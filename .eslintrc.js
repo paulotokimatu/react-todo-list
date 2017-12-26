b09ec304d368deedb2d717a7b8e48d417508a68d
paulotokimatu <paulomtf2005@yahoo.com.br>
2017-12-26 20:38:20 -0200

    Added .gitignore and config files for babel, eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "plugin:react/recommended"
+  ],
+  "rules": {
+    "linebreak-style": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  }
+};
